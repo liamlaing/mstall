991a6125565f974951c831377fd4c084c5236b0c
Liam Laing <liam.laing@canterbury.ac.nz>
2022-06-29 21:36:25 +1200

    adding a quick teardown build

--- mstall.sh
+++ mstall.sh
@@ -17,9 +17,10 @@ read VERSION
 git checkout MOODLE_${VERSION}_STABLE
 
 # plugins
-# Note the development hack for m4
-git clone -b development https://github.com/trampgeek/moodle-qtype_coderunner.git ${INSTALL_DIR}/${MOODLE_FOLDER}/question/type/coderunner
 git clone https://github.com/trampgeek/moodle-qbehaviour_adaptive_adapted_for_coderunner.git ${INSTALL_DIR}/${MOODLE_FOLDER}/question/behaviour/adaptive_adapted_for_coderunner
+
+git clone https://github.com/trampgeek/moodle-qtype_coderunner.git ${INSTALL_DIR}/${MOODLE_FOLDER}/question/type/coderunner
+
 git clone https://github.com/trampgeek/moodle-filter_ace_inline.git ${INSTALL_DIR}/${MOODLE_FOLDER}/filter/ace_inline
 
 
@@ -56,11 +57,19 @@ echo "database password"
 read DB_PASSWORD
 
 psql -U postgres -c "CREATE USER $DB_USERNAME WITH PASSWORD '${DB_PASSWORD}'";
-psql -U postgres -c "CREATE DATABASE ${DB_NAME} WITH OWNER ${DB_USERNAME};"
+psql -U postgres -c "CREATE DATABASE ${DB_NAME} WITH OWNER ${DB_USERNAME}"
 
 cd $INSTALL_DIR/${MOODLE_FOLDER}/admin/cli
-/usr/bin/php${PHP_VERSION} install.php
-
 
 chown -R root $INSTALL_DIR/${MOODLE_FOLDER}
 chmod -R 0755 $INSTALL_DIR/${MOODLE_FOLDER}
+
+# quick teardown
+# not including repo installs
+TEARDOWN_FILE="${INSTALL_DIR}/un.mstall.${MOODLE_FOLDER}.sh"
+echo '#!/usr/bin/env sh' >> $TEARDOWN_FILE
+echo "rm -r ${INSTALL_DIR}/${MOODLE_FOLDER}" >> $TEARDOWN_FILE
+echo "rm -r ${INSTALL_DIR}/${MOODLE_DATA_FOLDER}" >> $TEARDOWN_FILE
+echo psql -U postgres -c "'DROP DATABASE ${DB_NAME}'" >> $TEARDOWN_FILE
+echo  psql -U postgres -c "'DROP USER $DB_USERNAME '" >> $TEARDOWN_FILE
+chmod +x $TEARDOWN_FILE
